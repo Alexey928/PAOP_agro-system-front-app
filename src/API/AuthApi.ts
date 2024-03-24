6089d6cac18a4a08770c04ee3a1bcc6e392a4935
Alexey928 <aleksey__tihakov@ukr.net>
2024-03-24 15:28:40 +0300

    add router-dom and axios , created API arhetecture

---
+++ src/API/AuthApi.ts
@@ -0,0 +1,17 @@
+import {instance} from "./commonApiInstanse";
+
+export const authAPI = {
+    authMe() {
+        return instance.get<any>("auth/me");
+    },
+    login(email: string, password: string, rolle: boolean){
+        return instance.post<any>("auth/login", {
+            email,
+            password,
+            rolle,
+        });
+    },
+    logout() {
+        return instance.delete<any>("auth/login");
+    },
+};
